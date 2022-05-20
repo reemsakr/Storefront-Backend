917aab8861fb5317168294ff0c104ed9cbb8d7b0
reemsakr <reemsakr060@gmail.com>
2022-05-20 15:49:10 +0200

    final commit

--- src/routes/api/user_routes.ts
+++ src/routes/api/user_routes.ts
@@ -3,7 +3,7 @@ import * as handlers from '../../handlers/user_handler';
 import validateTokenMiddleware from '../../middlewares/authenticationmiddleware';
 const routes = Router();
 
-routes.post('/', validateTokenMiddleware, handlers.create);
+routes.post('/',handlers.create);
 routes.get('/', validateTokenMiddleware, handlers.index);
 routes.get('/:id', validateTokenMiddleware, handlers.show);
 routes.delete('/:id', validateTokenMiddleware, handlers.destroy);
